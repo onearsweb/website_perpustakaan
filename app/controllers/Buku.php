3e59adbe4a6bf7b304b035bcb7ad71df495a8bed
akusanfauriza <yafiatfacebook@gmail.com>
2024-07-09 23:40:09 +0700

    Nambah Function Edit & Update Data Buku

--- app/controllers/Buku.php
+++ app/controllers/Buku.php
@@ -1,5 +1,4 @@
 <?php
-
 class Buku extends Controller {
     public function index() {
         $data['title'] = "Data Buku";
@@ -10,14 +9,14 @@ class Buku extends Controller {
     }
 
     public function detail($id) {
-        $data['title'] = "Data Buku";
+        $data['title'] = "Detail Buku";
         $data['buku'] = $this->model('Buku_model')->getBukuById($id);
         $this->view('templates/header', $data);
         $this->view('buku/detail', $data);
         $this->view('templates/footer');
     }
 
-    // Function Query Insert
+    // Function Tambah Buku
     public function tambahBuku() {
         if ($this->model('Buku_model')->tambahDataBuku($_POST) > 0) {
             header('Location: ' . baseURL . '/buku');
@@ -25,11 +24,29 @@ class Buku extends Controller {
         }
     }
 
-    // Function Query Delete
-    public function hapusBuku($id){
+    // function Edit Buku
+    public function editBuku($id) {
+        $data['title'] = "Edit Data Buku";
+        $data['buku'] = $this->model('Buku_model')->getBukuById($id);
+        $this->view('templates/header', $data);
+        $this->view('buku/edit', $data);
+        $this->view('templates/footer');
+    }
+
+    // Function Update Buku
+    public function updateBuku() {
+        if ($this->model('Buku_model')->updateDataBuku($_POST) > 0) {
+            header('Location: ' . baseURL . '/buku');
+            exit;
+        }
+    }
+
+    // Function Hapus Buku
+    public function hapusBuku($id) {
         if ($this->model('Buku_model')->hapusDataBuku($id) > 0) {
             header('Location: ' . baseURL . '/buku');
             exit;
         }
     }
 }
+?>
