3e7c499347afd513a39ed93968a0b7e9d86fdeff
akusanfauriza <yafiatfacebook@gmail.com>
2024-07-11 16:47:47 +0700

    fix bug title karena harus memanggil parameter data di Home

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,8 +3,8 @@
 class Home extends Controller{
     public function index(){
         $data['title'] = "Dashboard";
-        $this->view('templates/header');
-        $this->view('home/index');
+        $this->view('templates/header', $data);
+        $this->view('home/index', $data);
         $this->view('templates/footer');
     }
 }
