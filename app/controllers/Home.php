cf846da9b42c27275d86b9e34c58a45797fd9678
R.Angel <adis.anjeli01@gmail.com>
2024-07-11 19:27:45 +0700

    fix:data tidak terkirim (debugging)

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,8 +1,34 @@
 <?php
 
-class Home extends Controller{
-    public function index(){
+class Home extends Controller
+{
+    public function index()
+    {
         $data['title'] = "Dashboard";
+
+        $buku = $this->model('Buku')->getAllBuku();
+        $pinjam = $this->model('Pinjam_model')->getAllPeminjaman();
+        $anggota = $this->model('Anggota_model')->getAllAnggota();
+
+        $totalStok = 0;
+        foreach ($buku as $b) {
+            $totalStok += $b['stok'];
+        }
+
+        $totalPinjam = 0;
+        foreach ($pinjam as $p) {
+            if ($p['status'] === 'Dipinjam') {
+                $totalPinjam += $p['jumlah_pinjam'];
+            }
+        }
+
+        $totalAnggota = count($anggota);
+
+        $data['bukuPinjam'] = $totalPinjam;
+        $data['sisaBuku'] = $totalStok;
+        $data['totalBuku'] = $totalStok + $totalPinjam;
+        $data['totalAnggota'] = $totalAnggota;
+
         $this->view('templates/header', $data);
         $this->view('home/index', $data);
         $this->view('templates/footer');
